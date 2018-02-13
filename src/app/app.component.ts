2caf4f39168c6b5406725ec8d5323d842a9df664
Jason White <jmw5598@gmail.com>
2018-02-13 13:19:03 -0500

    updated app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InMemoryCacheService } from './in-memory-cache.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  key: string;
+  value: string
+  search: string;
+
+  constructor(public cacheService: InMemoryCacheService) {}
+
+  ngOnInit() {}
+
+  push(key, value) {
+    if(key && value)
+      this.cacheService.store(key, value);
+  }
+
+  print(key) {
+    if(key)
+      console.log(this.cacheService.get(this.key));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import { InMemoryCacheService } from './in-memory-cache.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [InMemoryCacheService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
